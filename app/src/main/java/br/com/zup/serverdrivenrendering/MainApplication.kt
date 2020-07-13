ba9a360eae1c5c0513143d32504127e435efe30b
Gabriel Moraes <gabrielmoraesleonardo@gmail.com>
2020-07-13 17:01:53 -0300

    Create Koin Modules, Create MainApplication and starts Koin configuration

---
+++ app/src/main/java/br/com/zup/serverdrivenrendering/MainApplication.kt
@@ -0,0 +1,26 @@
+package br.com.zup.serverdrivenrendering
+
+import android.app.Application
+import br.com.zup.serverdrivenrendering.di.dataModule
+import br.com.zup.serverdrivenrendering.di.presentationModule
+import br.com.zup.serverdrivenrendering.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(
+                listOf(
+                    repositoryModule,
+                    dataModule,
+                    presentationModule
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/br/com/zup/serverdrivenrendering/di/Modules.kt
@@ -0,0 +1,38 @@
+package br.com.zup.serverdrivenrendering.di
+
+import br.com.zup.serverdrivenrendering.data.datasource.JsonProvider
+import br.com.zup.serverdrivenrendering.data.datasource.LayoutDataSource
+import br.com.zup.serverdrivenrendering.data.manager.JsonManager
+import br.com.zup.serverdrivenrendering.data.manager.LayoutDataSourceManager
+import br.com.zup.serverdrivenrendering.domain.LayoutRepository
+import br.com.zup.serverdrivenrendering.presentation.view.MainViewModel
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single {
+        LayoutRepository(
+            screenInfoMapper = get(),
+            layoutDataSource = get()
+        )
+    }
+}
+
+val dataModule = module {
+    single<LayoutDataSource> {
+        LayoutDataSourceManager(
+            jsonProvider = get(),
+            jsonManager = get()
+        )
+    }
+    factory { JsonProvider() }
+    factory { JsonManager() }
+}
+
+val presentationModule = module {
+    viewModel {
+        MainViewModel(
+            layoutRepository = get()
+        )
+    }
+}
